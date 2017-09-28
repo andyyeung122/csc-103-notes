da40c0a69f5fdd1c7bd220091e3effdaec12b75c
csc103 student <me@citymail.ccny.cuny.edu>
2017-09-28 11:47:30 -0400

    First commit

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+/* this basically copy pastes the contents of the iostream
+ * C++ file. This allows us to do the basic functionality of 
+ * C++ programs */
+using std::cout;
+/* NOTE: cout has a first name (cout_ and a last name
+ * which is (std). Without this "using" statement we
+ * would have to say "std:cout" instead of just "cout". 
+ * this is the starting point of the program: */
+
+int main() {
+	cout << "HELLO MY NAME IS NAMAN\n";
+	return 0;
+}
+
